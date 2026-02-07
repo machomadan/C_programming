0ad117e0bab75d9fd001a23d781a6a895af7b114
Rupanagudi Madan Mohan <machomadan@gmail.com>
2026-02-07 22:19:40 +0530

    To commit the changes in the C program related to strotok function usage which extracts the macs accordingly

---
+++ mac_split.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int mac_split(char * str, char *p[],int count){
+	char *ctx;
+	char *copy;
+	int i=0;
+	copy=strdup(str);
+	ctx=strtok(copy,",");
+	while(ctx!=NULL && i<count){
+		p[i]=strdup(ctx);
+		ctx=strtok(NULL,",");
+		i++;
+	}
+	return count;
+}
+
+int main(){
+	char a[]="00:11:22:33:44:55,AA:BB:CC:DD:EE:FF";
+	char *b,*new=a;
+	char *ch=',';
+	char *p;
+	int i=0,result;
+	while(ch=strchr(,',') !=NULL){
+		i++;
+		new++;
+	}
+	b=(char **)malloc(i*1);
+	result=mac_split(a,&b,i);
+	for(i=0;i<result;i++){
+		printf("mac value b[%d]=%s",i,b[i]);
+	}
+	return;
+}
+
+
